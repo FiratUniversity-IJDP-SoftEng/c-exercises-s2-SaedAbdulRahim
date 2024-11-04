90d71a4d69e24fd0586d3fd1331998506f90fee8
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2024-11-04 07:35:22 +0300

    ex07.c

--- ex07.c
+++ ex07.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("%-10s %-10s %-10s\n", "i*5", "100-((i-1)*10+1)", "i"); // Print header
+    printf("%-10s %-10s %-10s\n", "i*5", "100-((i-1)*10+1)", "i");
 
     for (int i = 1; i <= 10; i++) {
         printf("%-10d %-10d %-10d\n", i * 5, 100 - ((i - 1) * 10 + 1), i);
