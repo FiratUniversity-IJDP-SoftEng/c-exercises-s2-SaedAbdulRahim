c07425a8177b144dc26c038392a5df136b6a59f0
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2024-11-04 07:44:09 +0300

    ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    printf("Multiplication Table (10x10):\n\n");
+
+    printf("     ");
+    for (int i = 1; i <= 10; i++) {
+        printf("%4d", i);
+    }
+    printf("\n");
+
+    printf("    ");
+    for (int i = 1; i <= 10; i++) {
+        printf("----");
+    }
+    printf("\n");
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%4d |", i);
+        for (int j = 1; j <= 10; j++) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
