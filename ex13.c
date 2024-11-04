01d4d79327e803e25749ac385c4f06b2abfd9de3
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2024-11-04 07:48:41 +0300

    ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter a number (n < 50): ");
+    scanf("%d", &n);
+
+    if (n < 0 || n >= 50) {
+        printf("Please enter a number between 0 and 49.\n");
+        return 1;
+    }
+
+    unsigned long long result = factorial(n);
+
+    printf("Factorial of %d = %llu\n", n, result);
+
+    return 0;
+}
 
